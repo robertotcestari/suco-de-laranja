b7f76387fb3ef80a18f9b989d23dd5a8646cb98d
Roberto Cestari <robertotcestari@gmail.com>
2022-01-22 12:30:45 -0300

    fine adjustment

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ var inquirer = require("inquirer");
 const sucoDeLaranja = require("./sucoDeLaranja");
 
 const choices = [
-  { name: "Suco de Laranja, Hã, Hã...", value: "suco-de-laranja-2" },
-  { name: "Suco de Caju", value: "suco-de-laranja-3" },
-  {
-    name: "Por do sol, suco de laranja, f***-se as regras",
-    value: "suco-de-laranja-1",
-  },
+    { name: "Suco de Caju", value: "suco-de-laranja-3" },
+    {
+        name: "Por do sol, suco de laranja, f***-se as regras",
+        value: "suco-de-laranja-1",
+    },
+    { name: "Suco de Laranja (versão Gabriel)", value: "suco-de-laranja-2" },
 ];
 console.log(' ')
 
